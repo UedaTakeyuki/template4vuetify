4dea188415a377cc792cad332560a41d748243dc
Takeyuki UEDA <ueda@latelierdueda.com>
2019-08-24 12:02:55 +0900

    start line of Navigation Drawers

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <v-toolbar>
+      <v-toolbar-title class="grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  }),
+}
+</script>
