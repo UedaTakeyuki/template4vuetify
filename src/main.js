748d75d0d714c160d847cd29de516248853f5f85
Takeyuki UEDA <ueda@latelierdueda.com>
2020-01-28 08:39:09 +0900

    fb

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false;
 console.log(process.env.VUE_APP_AHO);
 console.log(process.env.AHO);
 Vue.prototype.$aho = process.env.VUE_APP_AHO;
+
+var config = {
+    apiKey: process.env.VUE_APP_apiKey,
+    authDomain: process.env.VUE_APP_authDomain,
+    databaseURL: process.env.VUE_APP_databaseURL,
+    projectId: process.env.VUE_APP_projectId,
+    storageBucket: process.env.VUE_APP_storageBucket,
+    messagingSenderId: process.env.VUE_APP_messagingSenderId
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    {{this.$aho}}
+<!--    {{this.$aho}} -->
   </div>
 </template>
 
